a64affcfeb2bd43089418530bbdc572470e2abb2
volx <97158976+vo1x@users.noreply.github.com>
2024-09-26 21:10:47 +0545

    fix: Switch to `.ico` icons

--- src/components/desktop/TaskbarIcon.tsx
+++ src/components/desktop/TaskbarIcon.tsx
@@ -1,16 +1,15 @@
 type TaskbarIconProps = {
   name: 'start' | 'explorer';
+  size?: number;
 };
 
-import StartIcon from '../icons/StartIcon';
-
-const TaskbarIcon: React.FC<TaskbarIconProps> = ({ name }) => {
-  if (name === 'start')
-    return (
-      <div className='p-2 rounded-lg hover:bg-neutral-200/15'>
-        <StartIcon />
-      </div>
-    );
+const TaskbarIcon: React.FC<TaskbarIconProps> = ({ name, size = 32 }) => {
+  const ICON = `/icons/${name}.ico`;
+  return (
+    <div className={`p-2 rounded-lg hover:bg-neutral-200/15`}>
+      <img src={ICON} alt="Start" className={``} width={`${size}`} />
+    </div>
+  );
 };
 
 export default TaskbarIcon;
